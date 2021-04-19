029f7ffb7c4ef1ff11c6e3a392fef2d975d3a7fd
keshav <skeshav162@gmail.com>
2021-04-19 21:46:08 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [sourceCurrency, setSourceCurrency] = useState('')
+  const [targetCurrency, setTargetCurrency] = useState('')
+  const [sourceVal, setSourceVal] = useState(Number);
+  const [targetVal, setTargetVal] = useState(Number);
+
+  console.log("Source", sourceCurrency);
+
+  const getResult = async () => {
+    
+    if(targetCurrency) {
+      const {data} = await axios.get(`https://open.exchangerate-api.com/v6/latest/${targetCurrency}`);
+      const val = data.rates
+      Object.keys(val)
+      .forEach(function eachKey(key) { 
+        if(key === sourceCurrency) {
+          setTargetVal((sourceVal * (1 / val[key])).toFixed(2))
+        }
+      });
+
+    }
+  }
+
+  useEffect(() => {
+    getResult()
+  },[targetCurrency, sourceVal, sourceCurrency])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="appHeader">
+        <span>{sourceVal} {sourceCurrency} equals</span>
+        <h1>{targetVal} {targetCurrency}</h1>
+      </div>
+      <div className="convertor">
+        <div className="source__currency">
+          <input type="text" placeholder="Source" onChange={(e) => setSourceVal(e.target.value)} />
+          <select name="currency" onChange={(e) => setSourceCurrency(e.target.value)}>  
+            <option value="null">Select</option>
+            <option value="USD">USD</option>
+            <option value="AED">AED</option>
+            <option value="GBP">GBP</option>
+            <option value="CAD">CAD</option>
+            <option value="SGD">SGD</option>
+            <option value="EUR">EUR</option>
+            <option value="JPY">JPY</option>
+            <option value="PKR">PKR</option>
+            <option value="ZAR">ZAR</option>
+            <option value="ALL">ALL</option>
+          </select>
+        </div>
+        <div className="target__currency">
+          <input type="text" placeholder="target" value={targetVal} readOnly />
+          <select name="currency" onChange={(e) => setTargetCurrency(e.target.value)}>
+            <option value="null">Select</option>
+            <option value="USD">USD</option>
+            <option value="AED">AED</option>
+            <option value="GBP">GBP</option>
+            <option value="CAD">CAD</option>
+            <option value="SGD">SGD</option>
+            <option value="EUR">EUR</option>
+            <option value="JPY">JPY</option>
+            <option value="PKR">PKR</option>
+            <option value="ZAR">ZAR</option>
+            <option value="ALL">ALL</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
